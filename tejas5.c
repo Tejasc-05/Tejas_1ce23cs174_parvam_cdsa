868ee76c7c058b426a75e489f845ed5f100f9174
Tejasc-05 <ctejas497@gmail.com>
2024-11-04 12:25:37 +0530

    Add files via upload

---
+++ tejas5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=20;
+    int b=30;
+    int c=a+b;
+    printf("%d\n",c);
+    int d=a*b;
+    printf("%d\n",d);
+    int e=a/b;
+    printf("%d\n",e);
+    int f=a%b;
+    printf("%d\n",f);
+
+}
