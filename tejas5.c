7651fc854d7d83999328d7e2272d832ac0a3134c
Tejasc-05 <ctejas497@gmail.com>
2024-11-04 12:32:36 +0530

    Add files via upload

--- tejas5.c
+++ tejas5.c
@@ -3,13 +3,13 @@ int main()
 {
     int a=20;
     int b=30;
-    int c=a+b;
-    printf("%d\n",c);
-    int d=a*b;
-    printf("%d\n",d);
-    int e=a/b;
-    printf("%d\n",e);
-    int f=a%b;
-    printf("%d\n",f);
+    int sum=a+b;
+    printf("%d\n",sum);
+    int mul=a*b;
+    printf("%d\n",mul);
+    int div=a/b;
+    printf("%d\n",div);
+    int mod=a%b;
+    printf("%d\n",mod);
 
 }
